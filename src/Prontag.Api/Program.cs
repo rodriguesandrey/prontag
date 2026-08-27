ca8456edb926af1c79470a5b32e555340fe3ef8b
Andreyrodrigues <113158950+rodriguesandrey@users.noreply.github.com>
2026-08-27 20:55:59 +0000

    Adiciona EF Core com SQLite, DbContext com filtro de tenant, migration inicial

--- src/Prontag.Api/Program.cs
+++ src/Prontag.Api/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Prontag.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ProntagDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITenantProvider, FixedTenantProvider>();
 
 var app = builder.Build();
 
