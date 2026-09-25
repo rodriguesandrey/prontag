03886091d808989e3831b2f87714ff5003d29698
Andrey <andrey.rspa@gmail.com>
2026-09-25 06:22:42 -0300

    Separa categorias de Produtos e Expositor em listas distintas

--- src/Prontag.Tablet/Categorias.cs
+++ src/Prontag.Tablet/Categorias.cs
@@ -2,12 +2,21 @@ namespace Prontag.Tablet;
 
 public static class Categorias
 {
-    public static readonly string[] Todas = new[]
+    public static readonly string[] Expositor = new[]
     {
         "Salgados",
         "Quitandas de queijo",
         "Quitandas doces",
         "Confeitaria",
         "Sequilhos"
+    };
+
+    public static readonly string[] Produtos = new[]
+    {
+        "Farinhas e grãos",
+        "Fermentos e químicos",
+        "Recheios e coberturas",
+        "Laticínios",
+        "Diversos"
     };
 }
